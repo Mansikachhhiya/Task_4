031f1e63347833f255a6521344b9f432dd0650a5
Mansikachhhiya <kachhiyamanshi1542810@gmail.com>
2022-06-11 22:32:52 +0530

    modify files

--- js_files/from_2.js
+++ js_files/from_2.js
@@ -1,14 +1,97 @@
+// event.preventDefault();
+let fname=document.getElementById("fname");
+let lname=document.getElementById("lname");
+let add1=document.getElementById("staddress");
+let add2=document.getElementById("staddress2");
+let city=document.getElementById("city");
+let state=document.getElementById("state");
+let code=document.getElementById("zipcode");
+let phoneno=document.getElementById("phoneno");
+let email= document.getElementById("email");
+let Text;
 function validate(){
+     event.preventDefault();
+    // let fname=document.getElementById("fname");
+    // let lname=document.getElementById("lname");
+    // let add1=document.getElementById("staddress");
+    // let add2=document.getElementById("staddress2");
+    // let city=document.getElementById("city");
+    // let state=document.getElementById("state");
+    // let code=document.getElementById("zipcode");
+    // let phoneno=document.getElementById("phoneno");
+    // let email= document.getElementById("email");
+    let Text1 ="Required";
+    if(fname.value=="")
+    {
+        
+       document.getElementById("p1").innerHTML=Text1;
+    }
+    
+    if(lname.value=="")
+    {
+        document.getElementById("p2").innerHTML=Text1;
+    }
+    
+    if(add1.value=="")
+    {  
+        
+        document.getElementById("p3").innerHTML=Text1;
+    }
+   
+    if(add2.value=="")
+    {
+        
+        document.getElementById("p4").innerHTML=Text1;
+    }
+   
+    if(city.value=="")
+    {
+        
+        document.getElementById("p5").innerHTML=Text1;
+    }
+    
+    if(state.value=="")
+    {
+        
+        document.getElementById("p6").innerHTML=Text1;
+    }
+    
+    if(code.value=="")
+    {
+       
+        document.getElementById("p7").innerHTML=Text1;
+    }
+    
+    if(phoneno.value=="")
+    {
+       
+        document.getElementById("p8").innerHTML=Text1;
+    }
+    
+    if(email.value=="")
+    {
+        
+        document.getElementById("p9").innerHTML=Text1;
+    }
+    let feedback= document.getElementById("feedback");
+    
+    if(feedback.value=="")
+    {
+       
+        document.getElementById("p10").innerHTML= Text1;
+    }
+    let suggestion= document.getElementById("suggestion");
+    
+    if(suggestion.value=="")
+    {
+       
+        document.getElementById("p11").innerHTML= Text1;
+    }
+
+}
+function fname_validate(){
     event.preventDefault();
     let fname=document.getElementById("fname");
-    let lname=document.getElementById("lname");
-    let add1=document.getElementById("staddress");
-    let add2=document.getElementById("staddress2");
-    let city=document.getElementById("city");
-    let state=document.getElementById("state");
-    let code=document.getElementById("zipcode");
-    let phoneno=document.getElementById("phoneno");
-    let email= document.getElementById("email");
     let Text;
     let name_pattren = "[a-zA-Z]{2,30}$";
     if(!fname.value.match(name_pattren))
@@ -16,54 +99,89 @@ function validate(){
         Text="please Enter valid name";
        document.getElementById("p1").innerHTML=Text;
     }
+
+}
+function lname_validate(){
+    event.preventDefault();
     let name2_pattren = "[a-zA-Z]{2,30}$";
-    if(!lname.value.match(name_pattren))
+    if(!lname.value.match(name2_pattren))
     {
         Text="please Enter valid Name";
         document.getElementById("p2").innerHTML=Text;
     }
-    let add1_pattren = "[a-zA-Z0-9]{2,100}$"
+
+}
+function add1_validate(){
+    event.preventDefault();
+    let add1_pattren = "[A-Za-z0-9'\.\-\s\,]{2,100}$"
     if(!add1.value.match(add1_pattren))
     {  
         Text="please Enter valid Address";
         document.getElementById("p3").innerHTML=Text;
     }
-    let add2_pattren = "[a-zA-Z0-9]{2,100}$"
+
+}
+function add2_validate(){
+    event.preventDefault();
+    let add2_pattren = "[A-Za-z0-9'\.\-\s\,]{2,100}$"
     if(!add2.value.match(add2_pattren))
     {
         Text="please Enter valid Address";
         document.getElementById("p4").innerHTML=Text;
     }
+}
+function city_validate(){
+    event.preventDefault();
     let city_pattern = "[a-zA-Z]{2,30}$";
     if(!city.value.match(city_pattern))
     {
         Text="please Enter valid city";
         document.getElementById("p5").innerHTML=Text;
     }
+}
+function state_validate(){
+    event.preventDefault();
     let state_pattern = "[a-zA-Z]{2,30}$";
-    if(!state.value.match(city_pattern))
+    if(!state.value.match(state_pattern))
     {
         Text="please Enter valid state";
         document.getElementById("p6").innerHTML=Text;
     }
-    let code_pattern="[1-9](2,6)$";
+
+}
+function code_valiadte(){
+    event.preventDefault();
+    let code_pattern="^[0-9]{6}$";
     if(!code.value.match(code_pattern))
     {
         Text="please Enter valid code";
         document.getElementById("p7").innerHTML=Text;
     }
+
+}
+function phoneno_validate(){
+    event.preventDefault();
     let phoneno_pattern="\(([0-9]{3})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{3})$";
     if(!phoneno.value.match(phoneno_pattern))
     {
         Text="please Enter valid phone no in the form of ((000) 0000-000)";
         document.getElementById("p8").innerHTML=Text;
     }
-    let email_pattren="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/";
+
+}
+function email_validate(){
+    event.preventDefault();
+    let email_pattren="/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/";
     if(!email.value.match(email_pattren))
     {
         Text="please Enter valid email id  in the form of ex:email@yahoo.com";
         document.getElementById("p9").innerHTML=Text;
     }
+
+}
+
+function feed_back(){
+    event.preventDefault();
     let feedback= document.getElementById("feedback");
     
     if(feedback.value=="")
@@ -71,6 +189,11 @@ function validate(){
        
         document.getElementById("p10").innerHTML="please Enter feed Back";
     }
+
+}
+function sugges_tion()
+{
+    event.preventDefault();
     let suggestion= document.getElementById("suggestion");
     
     if(suggestion.value=="")
@@ -78,25 +201,4 @@ function validate(){
        
         document.getElementById("p11").innerHTML="please Enter suggestion";
     }
-
 }
-// function feed_back(){
-//     let feedback= document.getElementById("feedback");
-    
-//     if(feedback.value=="")
-//     {
-       
-//         document.getElementById("p10").innerHTML="please Enter feed Back";
-//     }
-
-// }
-// function sugges_tion()
-// {
-//     let suggestion= document.getElementById("suggestion");
-    
-//     if(suggestion.value=="")
-//     {
-       
-//         document.getElementById("p11").innerHTML="please Enter suggestion";
-//     }
-// }
